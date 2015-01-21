b51fe66375538d4fbeed01fd626a10e381b887fb
Josh Ford <josh.ladon@yahoo.com>
2015-01-21 15:41:23 -0600

    add
    This program adds two numbers.

---
+++ Josh.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a = 5;
+  int b = 3;
+  int sum = 0;
+  
+  sum = a + b;
+  
+  cout << "The sum of " << a << " and " << b << " = " << sum << endl;
+  return 0;
+}
