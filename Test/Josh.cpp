ec8c925a1456072d4124416656d2788fc14a1a5e
Jeffrey <jeffrey@simonsolutions.com>
2015-01-22 23:31:26 -0600

    Tested automatic software documentation suite doc++.

--- Test/Josh.cpp
+++ Test/Josh.cpp
@@ -1,12 +1,60 @@
 #include <iostream>
 using namespace std;
-int main(){
-  int a = 5;
-  int b = 3;
-  int sum = 0;
-  
-  sum = a + b;
-  
-  cout << "The sum of " << a << " and " << b << " = " << sum << endl;
-  return 0;
+
+
+/**
+
+Author  : Joshua Ford & Jeffrey Allen
+
+Purpose : Testing github and doc++
+
+Date    : 1/21/2014
+
+blahblahblah
+
+*/
+int main() 
+{
+
+	int a = 5;
+	int b = 3;
+	int sum = 0;
+
+	sum = a + b;
+
+	cout << "The sum of " << a << " and " << b << " = " << sum << endl;
+	return 0;
+
 }
+
+/** Common base class
+
+	First class using doc++
+
+*/
+class TestClass
+{
+	private:
+		/// 1_privateComment
+		int testPrivate_1;
+		/// 2_privateComment
+		int testPrivate_2;
+		/// 3_privateComment
+		int testPrivate_3;
+
+	protected:
+		/// Double message?
+		char testProtected_1;
+		/// 2_protectedComment
+		char testProtected_2;
+		/// 3_protectedComment
+		char testProtected_3;
+
+	public:
+		/// 1_publicComment
+		bool testPublic_1;
+		/// 2_publicComment
+		bool testPublic_2;
+		/// 3_publicComment
+		bool testPublic_3;
+};
