a15a7ae9451a741c170285e831158acc7d2acf59
jeffrey allen <jeffreyallen92@gmail.com>
2015-03-07 23:09:07 -0600

    Jeffrey's First C# project

---
+++ Test/VisualStudioTest/FirstJeffreyTest/FirstJeffreyTest/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace FirstJeffreyTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.welcomeText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Location = new System.Drawing.Point(101, 196);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(75, 23);
+            this.HelloButton.TabIndex = 0;
+            this.HelloButton.Text = "Say Hello!";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.hello_Click);
+            // 
+            // nameText
+            // 
+            this.nameText.Location = new System.Drawing.Point(76, 61);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(100, 20);
+            this.nameText.TabIndex = 1;
+            // 
+            // welcomeText
+            // 
+            this.welcomeText.Location = new System.Drawing.Point(76, 109);
+            this.welcomeText.Name = "welcomeText";
+            this.welcomeText.Size = new System.Drawing.Size(100, 20);
+            this.welcomeText.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.welcomeText);
+            this.Controls.Add(this.nameText);
+            this.Controls.Add(this.HelloButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.TextBox welcomeText;
+    }
+}
+
