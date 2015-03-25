65351515cd5bef5f569cefd2b71ac25c89f326c8
Riley Smith <msmith9@una.edu>
2015-03-24 21:41:18 -0500

    Events

---
+++ LIFES/LIFES/UserInterfaces/MainGUI.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LIFES.UserInterfaces
+{
+    partial class MainGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Schedule = new System.Windows.Forms.Button();
+            this.Reschedule = new System.Windows.Forms.Button();
+            this.timeConstraintsButton = new System.Windows.Forms.Button();
+            this.enrollmentButton = new System.Windows.Forms.Button();
+            this.examTable = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textTest = new System.Windows.Forms.TextBox();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.examTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(22)))), ((int)(((byte)(107)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(719, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // Schedule
+            // 
+            this.Schedule.Location = new System.Drawing.Point(572, 387);
+            this.Schedule.Name = "Schedule";
+            this.Schedule.Size = new System.Drawing.Size(75, 23);
+            this.Schedule.TabIndex = 2;
+            this.Schedule.Text = "Schedule";
+            this.Schedule.UseVisualStyleBackColor = true;
+            // 
+            // Reschedule
+            // 
+            this.Reschedule.Location = new System.Drawing.Point(459, 387);
+            this.Reschedule.Name = "Reschedule";
+            this.Reschedule.Size = new System.Drawing.Size(75, 23);
+            this.Reschedule.TabIndex = 3;
+            this.Reschedule.Text = "Reschedule";
+            this.Reschedule.UseVisualStyleBackColor = true;
+            // 
+            // timeConstraintsButton
+            // 
+            this.timeConstraintsButton.Location = new System.Drawing.Point(75, 73);
+            this.timeConstraintsButton.Name = "timeConstraintsButton";
+            this.timeConstraintsButton.Size = new System.Drawing.Size(107, 23);
+            this.timeConstraintsButton.TabIndex = 4;
+            this.timeConstraintsButton.Text = "Time Constraints";
+            this.timeConstraintsButton.UseVisualStyleBackColor = true;
+            // 
+            // enrollmentButton
+            // 
+            this.enrollmentButton.Location = new System.Drawing.Point(208, 73);
+            this.enrollmentButton.Name = "enrollmentButton";
+            this.enrollmentButton.Size = new System.Drawing.Size(92, 23);
+            this.enrollmentButton.TabIndex = 5;
+            this.enrollmentButton.Text = "Total Enrollment";
+            this.enrollmentButton.UseVisualStyleBackColor = true;
+            // 
+            // examTable
+            // 
+            this.examTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.examTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.examTable.Location = new System.Drawing.Point(75, 140);
+            this.examTable.Name = "examTable";
+            this.examTable.Size = new System.Drawing.Size(572, 203);
+            this.examTable.TabIndex = 6;
+            this.examTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Class Time";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Exam Time";
+            this.Column2.Name = "Column2";
+            // 
+            // textTest
+            // 
+            this.textTest.Location = new System.Drawing.Point(89, 389);
+            this.textTest.Name = "textTest";
+            this.textTest.Size = new System.Drawing.Size(198, 20);
+            this.textTest.TabIndex = 7;
+            this.textTest.Text = "Text Area for Testing ";
+            this.textTest.TextChanged += new System.EventHandler(this.textTest_TextChanged);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(159)))), ((int)(((byte)(17)))));
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // MainGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(96)))), ((int)(((byte)(98)))));
+            this.ClientSize = new System.Drawing.Size(719, 471);
+            this.Controls.Add(this.textTest);
+            this.Controls.Add(this.examTable);
+            this.Controls.Add(this.enrollmentButton);
+            this.Controls.Add(this.timeConstraintsButton);
+            this.Controls.Add(this.Reschedule);
+            this.Controls.Add(this.Schedule);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainGUI";
+            this.Text = "LIons Final Exam Scheduler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.examTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button Schedule;
+        private System.Windows.Forms.Button Reschedule;
+        private System.Windows.Forms.Button timeConstraintsButton;
+        private System.Windows.Forms.Button enrollmentButton;
+        private System.Windows.Forms.DataGridView examTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox textTest;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
---
+++ LIFES/LIFES/UserInterfaces/MainGUI.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LIFES.UserInterfaces
+{
+    public partial class MainGUI : Form
+    {
+        public MainGUI()
+        {
+            InitializeComponent();
+            Schedule.Click += Schedule_Click;
+            Reschedule.Click += Reschedule_Click;
+            timeConstraintsButton.Click += timeConstraintsButton_Click;
+            enrollmentButton.Click += enrollmentButton_Click;
+        }
+
+        void enrollmentButton_Click(object sender, EventArgs e)
+        {
+            textTest.Text = "Clicked Total Enrollment Button";
+        }
+
+        void timeConstraintsButton_Click(object sender, EventArgs e)
+        {
+            textTest.Text = "Clicked Time Constraints Button";
+        }
+
+        void Reschedule_Click(object sender, EventArgs e)
+        {
+            textTest.Text = "Clicked Reschedule Button";
+        }
+
+        void Schedule_Click(object sender, EventArgs e)
+        {
+            examTable.Rows[0].Cells[0].Value = "First Class Time";
+            examTable.Rows[0].Cells[1].Value = "First Exam Time";
+ 
+            textTest.Text = "Clicked Schedule Button";
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textTest_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
