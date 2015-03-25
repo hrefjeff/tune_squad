096b154f13de78b0976606f5fd036427a1f4a307
ssmoke <ssmoke@una.edu>
2015-03-24 19:31:01 -0500

    Did some work on File Output

---
+++ LIFES/LIFES/FileIO/FileIn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFES.FileIO
+{
+    class FileIn
+    {
+        //currently a place holder until Josh adds his stuff
+    }
+}
---
+++ LIFES/LIFES/FileIO/FileOut.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+
+namespace LIFES.FileIO
+{
+    /*
+     * This class will output the final exam schedule to the requested 
+     * file type.
+     *  
+     * Author: Scott Smoke
+     * Last Modified by: Scott Smoke
+     */
+    class FileOut
+    {
+        private string filename;
+        public FileOut(string file)
+        {
+            filename = file;
+        }
+
+        /*
+         * This method will output the final exam schedule to a pdf. 
+         * This method uses the open source PDFSharp library by MigraDoc Foundation
+         * nore information here: http://www.pdfsharp.net/.
+         * 
+         * Sources: http://csharp.net-informations.com/file/create-pdf.htm
+         * 
+         * Author: Scott Smoke
+         * Modified by: Scott Smoke
+         */
+        public void writeToPDF()
+        {
+            //to do use pdfsharp
+            //PdfDocument pdf = new PdfDocument();
+            //PdfPage pdfPage = pdf.AddPage();
+            //XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+            //XFont font = new XFont("Times New Roman", 12);
+            //graph.DrawString("This is my first PDF document", font, XBrushes.Black,
+            //new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+            //pdf.Save("firstpage.pdf");
+
+        }
+        /*
+        * This method will output the final exam schedule to a csv. 
+        * 
+        * Author: Scott Smoke
+        * Modified by: Scott Smoke
+        */
+        public void writeToCSV()
+        {
+            //to do
+        }
+        /*
+        * This method will output the final exam schedule to a plain text file. 
+        * 
+        * Author: Scott Smoke
+        * Modified by: Scott Smoke
+        */
+        public void writeToText() 
+        {
+            //to do
+        }
+
+    }
+}
