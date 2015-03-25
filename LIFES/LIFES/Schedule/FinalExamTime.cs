d77b8c4630c41a7ae3db0c53861428b23d4d23b1
ssmoke <ssmoke@una.edu>
2015-03-24 19:52:34 -0500

    Created the schedule class and the finalExamTime class

---
+++ LIFES/LIFES/Schedule/FinalExamTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFES.Schedule
+{
+    /*
+     * This class represents a final exam time. This item
+     * contains all information about the final exam.
+     * 
+     * 
+     * Author: Scott Smoke
+     * Modified By: Scott Smoke 
+     */ 
+    class FinalExamTime
+    {
+    }
+}
---
+++ LIFES/LIFES/Schedule/Scedule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFES.Schedule
+{
+    class Scedule
+    {
+        /*
+         * This will construct a schedule object that will schedule the final 
+         * exams
+         * 
+         * 
+         * Author: Scott Smoke
+         * Modified by: Scott Smoke
+         * 
+         */
+        public void Schedule()
+        {
+            //to do 
+        }
+        /*
+         * This will reschedule the final exams once a user has pressed 
+         * the reschedule button.
+         * 
+         * Author: Scott Smoke
+         * Modified by: Scott Smoke 
+         * 
+         */
+        public void reSchedule()
+        {
+            //to do
+        }
+        
+    }
+}
