2b2526e25bf33b8ab61ca13761eab464570db3cd
ssmoke <ssmoke@una.edu>
2015-03-26 16:13:04 -0500

    Added some code

--- LIFES/LIFES/UserInterfaces/EnrollmentForm.Designer.cs
+++ LIFES/LIFES/UserInterfaces/EnrollmentForm.Designer.cs
@@ -36,9 +36,10 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(30, 37);
+            this.radioButton1.Location = new System.Drawing.Point(40, 46);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(41, 17);
+            this.radioButton1.Size = new System.Drawing.Size(51, 21);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Fall";
@@ -48,9 +49,10 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(95, 37);
+            this.radioButton2.Location = new System.Drawing.Point(127, 46);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.Size = new System.Drawing.Size(70, 21);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Spring";
@@ -58,21 +60,24 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(30, 101);
+            this.button1.Location = new System.Drawing.Point(40, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = "Choose File";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // EnrollmentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 166);
+            this.ClientSize = new System.Drawing.Size(379, 204);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "EnrollmentForm";
             this.Text = "EnrollmentForm";
             this.ResumeLayout(false);
--- LIFES/LIFES/UserInterfaces/EnrollmentForm.cs
+++ LIFES/LIFES/UserInterfaces/EnrollmentForm.cs
@@ -21,5 +21,24 @@ namespace LIFES.UserInterfaces
         {
 
         }
+
+        /*
+         * Method:Button1_click
+         * Pasrameters: object sender, EventArgs e
+         * Ouytput: N/A
+         * Created By: Scott Smoke
+         * Date: 3/26/2015
+         * Modified By: Scott Smoke
+         * This will pop up an open file dialog when clicked.
+         * This will allow the user to select a total enrollments file.
+         */ 
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Comma Seperated Values |*.csv";
+            openFile.Title = "Open Total Enrollments File";
+            openFile.ShowDialog();
+            Globals.totalEnrollemntsFileName = openFile.FileName;
+        }
     }
 }
