e60673a04dadf3e5b6ce52efc352d18fb3b8332e
ssmoke <ssmoke@una.edu>
2015-03-26 16:12:46 -0500

    Globals class containing all global variables

---
+++ LIFES/LIFES/Globals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIFES
+{
+    /*
+     * Class Name: Globals
+     * Created By: Scott Smoke
+     * Date: 3/26/2015
+     * Modified BY: Scott Smoke
+     * This class will contain all the global 
+     * variables that are used throughout the project.
+     */ 
+    public static class  Globals
+    {
+        public static string timeConstraintsFileName;
+        public static string totalEnrollemntsFileName;
+        public static TimeConstraints timeConstraints;
+        //Save file if applicable
+        //final exam schedule
+    }
+}
