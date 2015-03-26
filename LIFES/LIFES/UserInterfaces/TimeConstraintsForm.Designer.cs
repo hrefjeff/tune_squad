b84bf5dd9dcb7227c62d5c9f67366cc079ff9d19
ssmoke <ssmoke@una.edu>
2015-03-25 20:42:00 -0500

    Added comments

--- LIFES/LIFES/UserInterfaces/TimeConstraintsForm.Designer.cs
+++ LIFES/LIFES/UserInterfaces/TimeConstraintsForm.Designer.cs
@@ -44,107 +44,120 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(33, 27);
+            this.button1.Location = new System.Drawing.Point(44, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Chose File";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(176, 85);
+            this.textBox1.Location = new System.Drawing.Point(235, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(176, 131);
+            this.textBox2.Location = new System.Drawing.Point(235, 161);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(176, 172);
+            this.textBox3.Location = new System.Drawing.Point(235, 212);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(110, 20);
+            this.textBox3.Size = new System.Drawing.Size(145, 22);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(176, 212);
+            this.textBox4.Location = new System.Drawing.Point(235, 261);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(110, 20);
+            this.textBox4.Size = new System.Drawing.Size(145, 22);
             this.textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(176, 252);
+            this.textBox5.Location = new System.Drawing.Point(235, 310);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(110, 20);
+            this.textBox5.Size = new System.Drawing.Size(145, 22);
             this.textBox5.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 92);
+            this.label1.Location = new System.Drawing.Point(40, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.Size = new System.Drawing.Size(171, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Number of Days of Exams";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 138);
+            this.label2.Location = new System.Drawing.Point(40, 170);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.Size = new System.Drawing.Size(124, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Time of First Exam";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 179);
+            this.label3.Location = new System.Drawing.Point(40, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.Size = new System.Drawing.Size(113, 17);
             this.label3.TabIndex = 8;
             this.label3.Text = "Length of Exams";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 219);
+            this.label4.Location = new System.Drawing.Point(40, 270);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.Size = new System.Drawing.Size(155, 17);
             this.label4.TabIndex = 9;
             this.label4.Text = "Length Between Exams";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(30, 259);
+            this.label5.Location = new System.Drawing.Point(40, 319);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(118, 13);
+            this.label5.Size = new System.Drawing.Size(156, 17);
             this.label5.TabIndex = 10;
             this.label5.Text = "Length of Lunch Period";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(176, 27);
+            this.button2.Location = new System.Drawing.Point(235, 33);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.Size = new System.Drawing.Size(147, 28);
             this.button2.TabIndex = 11;
             this.button2.Text = "Update Constraints";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // TimeConstraintsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 308);
+            this.ClientSize = new System.Drawing.Size(429, 379);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -157,6 +170,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TimeConstraintsForm";
             this.Text = "TimeConstraintsForm";
             this.ResumeLayout(false);
--- LIFES/LIFES/UserInterfaces/TimeConstraintsForm.cs
+++ LIFES/LIFES/UserInterfaces/TimeConstraintsForm.cs
@@ -10,11 +10,58 @@ using System.Windows.Forms;
 
 namespace LIFES.UserInterfaces
 {
+    
     public partial class TimeConstraintsForm : Form
     {
+        private TimeConstraints tc;
         public TimeConstraintsForm()
         {
             InitializeComponent();
+        
+        }
+        /*
+         * Method: GetTimeConstraints
+         * Parameters: N/A
+         * Output: A TimeConstraints object
+         * Created By: Scott Smoke
+         * Date: 3/25/2015
+         * Modified By: Scott Smoke
+         * This will return the time constraints that have been entered.
+         * If no data was entered then it will construct an object with all zzeroes.
+         */ 
+        public TimeConstraints GetTimeConstraints()
+        {
+            return tc;
+        }
+        /*
+         * Method: button_2Click
+         * Paramters: object Sender, EventArgs e
+         * Output: N/A
+         * Created By: Scott Smoke
+         * Date: 3/25/2015
+         * Modified By: Scott Smoke
+         * When this button is clicked the data that is in the form
+         * get saved into the TimeConstraints variable.
+         * If no data is entered then the TimeConstraints variable
+         * data is all set to zero
+         * 
+         */ 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") && (textBox2.Text != "") &&
+                (textBox3.Text != "") && (textBox4.Text != "") &&
+                (textBox5.Text != ""))
+            {
+                TimeConstraints t = new TimeConstraints(Convert.ToInt32(textBox1.Text),
+                Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text),
+                Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox5.Text));
+                 tc = t;
+            }
+            else
+            {
+                TimeConstraints t = new TimeConstraints(0, 0, 0, 0, 0);
+                 tc = t;
+            }
         }
     }
 }
