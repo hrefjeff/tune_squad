e0de323d521d1388506e9fea196e2ec1b4e32761
jeffrey allen <jeffreyallen92@gmail.com>
2015-04-08 14:58:20 -0500

    Added to Scheduler namespace - Classes added: 'Class', 'CompressedClasses', 'MaxbinaryHeapOfClasses'

--- LIFES/LIFES/Schedule/FinalExamTime.cs
+++ LIFES/LIFES/Schedule/FinalExamTime.cs
@@ -17,4 +17,82 @@ namespace LIFES.Schedule
     class FinalExamTime
     {
     }
+
+    /*
+     * Class Name: MaxBinaryHeapOfClasses
+     * Author: Jeffrey Allen
+     * Date: 4/8/2015
+     * Modified by: Jeffrey Allen
+     * This class is the final exam times that
+     * are constructed after the scheduler has ran.
+     */
+    class MaxBinaryHeapOfClasses
+    {
+        public List<CompressedClasses> listOfCompressedClasses;
+
+        public void insert(Class someClass)
+        {
+        }
+    }
+
+    /*
+     * Class Name: CompressedClasses
+     * Author: Jeffrey Allen
+     * Date: 4/8/2015
+     * Modified by: Jeffrey Allen
+     * This class is the final exam times that
+     * are constructed after the scheduler has ran.
+     */ 
+    class CompressedClasses
+    {
+        private List<Class> listOfClasses;
+        private int _totalEnrollments;
+        private int _totalNumberOfClasses;
+
+        public void insertClass() { }
+        public void removeClass() { }
+        public void summary() { }
+    }
+
+    /*
+    * Class Name: Class
+    * Author: Jeffrey Allen
+    * Date: 4/8/2015
+    * Modified by: Jeffrey Allen
+    * This class is the final exam times that
+    * are constructed after the scheduler has ran.
+    */ 
+    class Class
+    {
+        private string _meetingDay;
+        private string _startTime;
+        private string _endTime;
+        private int _enrollment;
+
+        public string meetingDay
+        {
+            get { return _meetingDay; }
+            set { _meetingDay = value; }
+        }
+
+        public string startTime
+        {
+            get { return _startTime; }
+            set { _startTime = value; }
+        }
+
+        public string endTime
+        {
+            get { return _endTime; }
+            set { _endTime = value; }
+        }
+
+        public int enrollment
+        {
+            get { return _enrollment; }
+            set { _enrollment = value; }
+        }
+
+    }
+
 }
