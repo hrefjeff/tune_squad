05883a7c7490c3d488edd47affc3d3fb40a7d010
Josh Ford <josh.ladon@yahoo.com>
2015-04-13 22:07:52 -0500

    Added class compression
    This commit contains the class compressor. This does not compress class
    times as far as would be ideal. This is what we were given to work with.
    Due to only the class start time being the deciding factor in
    differentiating day classes from night classes. This is pretty much all
    I can do with this method of compression.

---
+++ LIFES/LIFES/FileIO/FinalExamScheduler/FinalExamScheduler/ClassTime.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FinalExamScheduler
+{
+    /*
+     * Class Name: ClassTime.cs
+     * Author: Joshua Ford, Shawn Weeks.
+     * Date: 3/28/15
+     * Modified by: Joshua Ford, Shawn Weeks.
+     * Description: Validate the data from the class times file is correct.
+     *              Raise errors on any that does not conform to day order,
+     *              minimum class start time, reasonable class end times, and
+     *              has an incorrect number of students (i.e. students < 1).
+     */ 
+    public class ClassTime
+    {
+        private readonly String dayOfTheWeek;
+        private readonly int classStartTime;
+        private readonly int classEndTime;
+        private readonly int studentsEnrolled;
+        private String ownedBy;
+
+        /*
+         * Method Name: ClassTime
+         * Parameters: _dayOfTheWeek    - The number of days that the given 
+         *                                class takes place.
+         *             
+         *             _classStartTime  - The time in which the class starts.
+         *            
+         *             _classEndTime    - The time in which the class ends.
+         *             
+         *             _studentsEnrolled- The number of students enrolled in 
+         *                                this class.
+         *             
+         * Output: No explicit output.
+         * Author: Joshua Ford, Shawn Weeks.
+         * Date: 3/28/15
+         * Modified By: Joshua Ford, Shawn Weeks.
+         * Description: This is the constructor: Checks line for correct 
+         *              class data.
+         */ 
+        public ClassTime(String _dayOfTheWeek, int _classStartTime, 
+            int _classEndTime, int _studentsEnrolled)
+        {
+            Regex r = new Regex("^M?T?W?R?F?$");
+            Match m = r.Match(_dayOfTheWeek);
+
+            if (!m.Success)
+            {
+                throw new Exception("Error - Days of the Week Invalid");
+            }
+            else if (_classStartTime < 700)
+            {
+                throw new Exception("Error - Class Start Time Before 0700");
+            }
+            else if (_classStartTime >= 1800)
+            {
+                throw new Exception("Error - Class Start Time After 1800");
+            }
+            else if (_classEndTime < _classStartTime)
+            {
+                throw new Exception("Error - Class Ends Before It Starts");
+            }
+            else if (_studentsEnrolled < 1)
+            {
+                throw new Exception("Error - Students Enrolled Less Than 1");
+            }
+
+            this.dayOfTheWeek = _dayOfTheWeek;
+            this.classStartTime = _classStartTime;
+            this.classEndTime = _classEndTime;
+            this.studentsEnrolled = _studentsEnrolled;
+            this.ownedBy = "NA";
+        }
+
+        // Setter for OwnedBy.
+        public void setOwnedBy(String _ownedBy)
+        {
+            this.ownedBy = _ownedBy;
+        }
+
+        // Getter for DayOfTheWeek.
+        public String getDayOfTheWeek()
+        {
+            return dayOfTheWeek;
+        }
+
+        // Getter for ClassStartTime.
+        public int getClassStartTime()
+        {
+            return classStartTime;
+        }
+
+        // Getter for ClassEndTime.
+        public int getClassEndTime()
+        {
+            return classEndTime;
+        }
+
+        // Getter for StudentsEnrolled.
+        public int getStudentsEnrolled()
+        {
+            return studentsEnrolled;
+        }
+
+        // Getter for OwnedBy.
+        public String getOwnedBy()
+        {
+            return ownedBy;
+        }
+    }
+}
+
---
+++ LIFES/LIFES/FileIO/FinalExamScheduler/FinalExamScheduler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalExamScheduler
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String fileName = "";
+            while (fileName == "")
+            {				
+                Console.Write("Enter your filename. ");
+                fileName = Console.ReadLine();
+            }
+
+            fileName = "C:\\Users\\Josh Ford\\Desktop\\fall_2014.csv";
+
+            var compressedClassTimes = new CompressedClassTimes(fileName);
+
+            int lineCount = 0;
+            foreach (var c in compressedClassTimes.getCompressedClassTimes())
+            {
+                lineCount++;
+                Console.WriteLine("Day\tStart Time\tTotal " + 
+                    "Students\tAssociated Classes");
+                Console.WriteLine(lineCount + " " + c.getDayOfTheWeek() + "\t"
+                    + c.getClassTimeStartHour() + ":00\t\t" + 
+                    c.getTotalStudentsEnrolled() + "\t\t" 
+                    + c.getClassTimes().Count);
+                Console.WriteLine();
+                Console.WriteLine("\tDay\tStart\tEnd\tStudents");
+                foreach (var c1 in c.getClassTimes())
+                {
+                    Console.WriteLine("\t" + c1.getDayOfTheWeek() 
+                        + "\t" + c1.getClassStartTime() + "\t" 
+                        + c1.getClassEndTime() + "\t" 
+                        + c1.getStudentsEnrolled());
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("Total Number of Compressed Class Times " 
+                + compressedClassTimes.getCompressedClassTimes().Count);
+            Console.ReadLine();
+        }
+    }
+}
