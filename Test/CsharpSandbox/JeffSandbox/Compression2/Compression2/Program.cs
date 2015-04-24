52f3b2f82019d8d58add3198a03baf482cc8a5d1
jeffrey allen <jeffreyallen92@gmail.com>
2015-04-23 19:09:28 -0500

    Updated User Manual. Updated meeting minutes for 4-23. Updated work log.

---
+++ Test/CsharpSandbox/JeffSandbox/Compression2/Compression2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetaExpr;
+
+namespace Compression2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    internal sealed class Solver
+    {
+      private enum Result { Solved, Unsolved, Busted }
+      private readonly Matrix conflictMatrix;
+      private readonly BitArray[] possibilities;
+      private readonly int colours;
+
+      public Solver(Matrix conflictMatrix, int colours)
+      {
+        this.colours = colours;
+        this.conflictMatrix = conflictMatrix;
+        this.possibilities = new BitArray[conflictMatrix.Rows];
+        BitArray b = new BitArray[];
+        for (int i = 0; i < colours; ++i)
+            b = b.Add(i);
+        for (int p = 0; p < this.possibilities.Length; ++p)
+            this.possibilities[p] = b;
+     }
+}
