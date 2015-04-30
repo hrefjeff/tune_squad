9760a0f4b459dd0b8a19c9f8791583ea1038e24c
ssmoke <ssmoke@una.edu>
2015-04-30 17:55:07 -0500

    Updated class

--- LIFES/LIFES/Schedule/Scheduler.cs
+++ LIFES/LIFES/Schedule/Scheduler.cs
@@ -5,42 +5,79 @@ using System.Text;
 using System.Threading.Tasks;
 using LIFES;
 using System.Collections;
+using LIFES.FileIO;
 namespace LIFES.Schedule
 {
     class Scheduler
     {
         private TimeConstraints tc;
-        //compressed data
+        private List<CompressedClassTime> compressedClassTime;
+
+        private int examSlots;
+
+        private void runScheduler()
+        {
+
+
+        }
+        private void AvailableExamSlots()
+        {
+            examSlots = (int)(((1715-tc.GetLunchPeriod()) - tc.GetStartTime()) / (tc.GetLengthOfExams() + tc.GetTimeBetweenExams()));
+             
+        }
         /*
-         * This will construct a schedule object that will schedule the final 
-         * exams
+         * Method: Scheduler
+         * Parameters: List<CompressedClassTime> ct, TimeConstraints t
+         * OutPut: N/A
+         * Created By: Scott Smoke
+         * Date: 4/30/2015
+         * Modified By: Scott Smoke
          * 
-         * 
-         * Author: Scott Smoke
-         * Modified by: Scott Smoke
-         * 
+         * Description: This is the constructor for the class.
          */
-        public Scheduler(TimeConstraints t)
+        public Scheduler(List<CompressedClassTime> ct, TimeConstraints t)
         {
             tc = t;
-
+            compressedClassTime = ct;
             //to do 
         }
-
-     
         /*
-         * This will reschedule the final exams once a user has pressed 
-         * the reschedule button.
+         * Method: reSchedule
+         * Parameters: N/A
+         * OutPut: N/A
+         * Created By: Scott Smoke
+         * Date: 4/30/2015
+         * Modified By: Scott Smoke
          * 
-         * Author: Scott Smoke
-         * Modified by: Scott Smoke 
-         * 
+         * Description: Calls the reScheduler
          */
-        public void reSchedule()
+        public void ReSchedule()
         {
             //to do
         }
 
-        
+        /*
+         * Method: scheduler
+         * Parameters: N/A
+         * Output: N/A
+         * Created By: Scott Smoke
+         * Date: 4/30/2015
+         * Modified By: Scott Smoke
+         * 
+         * Description: Calls the Scheduler.
+         * 
+         */ 
+        public void Schedule()
+        {
+
+            runScheduler();
+        }
+        public int GetExamSlots()
+        {
+            return examSlots;
+        }
+
+
+
     }
 }
